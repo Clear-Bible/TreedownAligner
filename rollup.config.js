66b48fe84cb0abce357959cf5b56acd5b2f2a823
Mike Brinker <themikejr@gmail.com>
2022-04-06 12:20:31 -0400

    Cleanup dependencies and rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,15 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-//import postcss from 'rollup-plugin-postcss';
-import styles from 'rollup-plugin-styles';
 import css from 'rollup-plugin-import-css';
-
-const packageJson = require('./package.json');
 
 export default {
   input: 'src/bundle.ts',
   output: [
     {
-      //file: packageJson.main,
       format: 'cjs',
       sourcemap: true,
       dir: 'dist',
     },
-    //{
-    //file: packageJson.module,
-    //format: 'esm',
-    //sourcemap: true,
-    //dir: 'dist',
-    //},
   ],
-  plugins: [
-    //peerDepsExternal(),
-    //resolve(),
-    //commonjs(),
-    typescript(),
-    css(),
-  ],
+  plugins: [typescript(), css()],
   external: ['react', 'react-dom'],
 };
