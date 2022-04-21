188b337d4d651aa846db63d91bc47e7689dabdd0
Mike Brinker <themikejr@gmail.com>
2022-04-21 11:05:26 -0400

    Introduce AligmmentPolarity.
    AlignmentPolarity is an object that disambiguates the sides
    of an alignment datum, allowing syntax mapping to take place.
    It works when provided to syntaxMapper, but the callers of
    syntaxMapper are now broken.

--- src/features/treedown/syntaxMapper.test.ts
+++ src/features/treedown/syntaxMapper.test.ts
@@ -5,11 +5,22 @@ import syntaxMapper from 'features/treedown/syntaxMapper';
 
 describe('syntaxMapper', () => {
   it('can map a single aligned word', () => {
-    const result = syntaxMapper(treedownSyntax as unknown as SyntaxRoot, {
-      target: 'an interesting greek source',
-      source: 'a very good english translation',
-      links: [{ targets: ['410160010010010'], sources: ['en_6'] }],
-    });
+    const result = syntaxMapper(
+      treedownSyntax as unknown as SyntaxRoot,
+      {
+        target: 'an interesting greek source',
+        source: 'a very good english translation',
+        links: [{ targets: ['410160010010010'], sources: ['en_6'] }],
+      },
+      null,
+      {
+        primary: {
+          syntaxSide: 'targets',
+          nonSyntaxSide: 'sources',
+        },
+        secondary: {},
+      }
+    );
 
     expect(
       result.children[0].children[0].children[0].content.alignedWordIds
@@ -17,13 +28,24 @@ describe('syntaxMapper', () => {
   });
 
   it('can map three aligned words', () => {
-    const result = syntaxMapper(treedownSyntax as unknown as SyntaxRoot, {
-      target: 'an interesting greek source',
-      source: 'a very good english translation',
-      links: [
-        { targets: ['410160010010010'], sources: ['en_4', 'en_5', 'en_7'] },
-      ],
-    });
+    const result = syntaxMapper(
+      treedownSyntax as unknown as SyntaxRoot,
+      {
+        target: 'an interesting greek source',
+        source: 'a very good english translation',
+        links: [
+          { targets: ['410160010010010'], sources: ['en_4', 'en_5', 'en_7'] },
+        ],
+      },
+      null,
+      {
+        primary: {
+          syntaxSide: 'targets',
+          nonSyntaxSide: 'sources',
+        },
+        secondary: {},
+      }
+    );
 
     expect(
       result.children[0].children[0].children[0].content.alignedWordIds
@@ -32,16 +54,31 @@ describe('syntaxMapper', () => {
 
   describe('link refactoring', () => {
     it('refactors many:many links into 1:many (symmetric)', () => {
-      const result = syntaxMapper(treedownSyntax as unknown as SyntaxRoot, {
-        target: 'an interesting greek source',
-        source: 'a very good english translation',
-        links: [
-          {
-            targets: ['410160010010010', '410160010020010', '410160010030010'],
-            sources: ['en_4', 'en_5', 'en_7'],
+      const result = syntaxMapper(
+        treedownSyntax as unknown as SyntaxRoot,
+        {
+          target: 'an interesting greek source',
+          source: 'a very good english translation',
+          links: [
+            {
+              targets: [
+                '410160010010010',
+                '410160010020010',
+                '410160010030010',
+              ],
+              sources: ['en_4', 'en_5', 'en_7'],
+            },
+          ],
+        },
+        null,
+        {
+          primary: {
+            syntaxSide: 'targets',
+            nonSyntaxSide: 'sources',
           },
-        ],
-      });
+          secondary: {},
+        }
+      );
 
       expect(
         result.children[0].children[0].children[0].content.alignedWordIds
@@ -59,16 +96,31 @@ describe('syntaxMapper', () => {
     });
 
     it('refactors many:many links into 1:many (asymmetric)', () => {
-      const result = syntaxMapper(treedownSyntax as unknown as SyntaxRoot, {
-        target: 'an interesting greek source',
-        source: 'a very good english translation',
-        links: [
-          {
-            targets: ['410160010010010', '410160010020010', '410160010030010'],
-            sources: ['en_4', 'en_5', 'en_7', 'en_8'],
+      const result = syntaxMapper(
+        treedownSyntax as unknown as SyntaxRoot,
+        {
+          target: 'an interesting greek source',
+          source: 'a very good english translation',
+          links: [
+            {
+              targets: [
+                '410160010010010',
+                '410160010020010',
+                '410160010030010',
+              ],
+              sources: ['en_4', 'en_5', 'en_7', 'en_8'],
+            },
+          ],
+        },
+        null,
+        {
+          primary: {
+            syntaxSide: 'targets',
+            nonSyntaxSide: 'sources',
           },
-        ],
-      });
+          secondary: {},
+        }
+      );
 
       expect(
         result.children[0].children[0].children[0].content.alignedWordIds
@@ -86,16 +138,31 @@ describe('syntaxMapper', () => {
     });
 
     it('refactors many:many links into 1:many (asymmetric surplus)', () => {
-      const result = syntaxMapper(treedownSyntax as unknown as SyntaxRoot, {
-        target: 'an interesting greek source',
-        source: 'a very good english translation',
-        links: [
-          {
-            targets: ['410160010010010', '410160010020010', '410160010030010'],
-            sources: ['en_4', 'en_5', 'en_7', 'en_8'],
+      const result = syntaxMapper(
+        treedownSyntax as unknown as SyntaxRoot,
+        {
+          target: 'an interesting greek source',
+          source: 'a very good english translation',
+          links: [
+            {
+              targets: [
+                '410160010010010',
+                '410160010020010',
+                '410160010030010',
+              ],
+              sources: ['en_4', 'en_5', 'en_7', 'en_8'],
+            },
+          ],
+        },
+        null,
+        {
+          primary: {
+            syntaxSide: 'targets',
+            nonSyntaxSide: 'sources',
           },
-        ],
-      });
+          secondary: {},
+        }
+      );
 
       expect(
         result.children[0].children[0].children[0].content.alignedWordIds
@@ -113,16 +180,31 @@ describe('syntaxMapper', () => {
     });
 
     it('refactors many:many links into 1:many (asymmetric deficit)', () => {
-      const result = syntaxMapper(treedownSyntax as unknown as SyntaxRoot, {
-        target: 'an interesting greek source',
-        source: 'a very good english translation',
-        links: [
-          {
-            targets: ['410160010010010', '410160010020010', '410160010030010'],
-            sources: ['en_4', 'en_5'],
+      const result = syntaxMapper(
+        treedownSyntax as unknown as SyntaxRoot,
+        {
+          target: 'an interesting greek source',
+          source: 'a very good english translation',
+          links: [
+            {
+              targets: [
+                '410160010010010',
+                '410160010020010',
+                '410160010030010',
+              ],
+              sources: ['en_4', 'en_5'],
+            },
+          ],
+        },
+        null,
+        {
+          primary: {
+            syntaxSide: 'targets',
+            nonSyntaxSide: 'sources',
           },
-        ],
-      });
+          secondary: {},
+        }
+      );
 
       expect(
         result.children[0].children[0].children[0].content.alignedWordIds
@@ -140,16 +222,27 @@ describe('syntaxMapper', () => {
     });
 
     it('does not duplicate 1 many:1 link to multiple', () => {
-      const result = syntaxMapper(treedownSyntax as unknown as SyntaxRoot, {
-        target: 'an interesting greek source',
-        source: 'a very good english translation',
-        links: [
-          {
-            targets: ['410160010010010', '410160010020010'],
-            sources: ['en_4'],
+      const result = syntaxMapper(
+        treedownSyntax as unknown as SyntaxRoot,
+        {
+          target: 'an interesting greek source',
+          source: 'a very good english translation',
+          links: [
+            {
+              targets: ['410160010010010', '410160010020010'],
+              sources: ['en_4'],
+            },
+          ],
+        },
+        null,
+        {
+          primary: {
+            syntaxSide: 'targets',
+            nonSyntaxSide: 'sources',
           },
-        ],
-      });
+          secondary: {},
+        }
+      );
 
       expect(
         result.children[0].children[0].children[0].content.alignedWordIds
@@ -163,7 +256,7 @@ describe('syntaxMapper', () => {
   });
 
   describe('mapping secondary alignments', () => {
-    it('works', () => {
+    it('secondarily maps a single word', () => {
       const result = syntaxMapper(
         treedownSyntax as unknown as SyntaxRoot,
         {
@@ -180,6 +273,17 @@ describe('syntaxMapper', () => {
           source: 'a very good english translation',
           target: 'an excellent spanish translation',
           links: [{ targets: ['spa_2'], sources: ['en_4'] }],
+        },
+        {
+          primary: {
+            syntaxSide: 'targets',
+            nonSyntaxSide: 'sources',
+          },
+
+          secondary: {
+            mappedSide: 'sources',
+            nonMappedSide: 'targets',
+          },
         }
       );
 
@@ -191,6 +295,45 @@ describe('syntaxMapper', () => {
       //result.children[0].children[0].children[1].children[0].content
       //.alignedWordIds
       //).toEqual([undefined]);
+    });
+
+    it('secondarily maps a single word (reversed)', () => {
+      // Greek (ms) <> Spanish (trans) <> English (lwc)
+      const result = syntaxMapper(
+        treedownSyntax as unknown as SyntaxRoot,
+        {
+          source: 'nestle1904',
+          target: 'nvi',
+          links: [
+            {
+              sources: ['410160010010010', '410160010020010'],
+              targets: ['nvi_2'],
+            },
+          ],
+        },
+        {
+          source: 'leb',
+          target: 'nvi',
+          links: [{ sources: ['en_4'], targets: ['nvi_2'] }],
+        },
+        {
+          primary: {
+            syntaxSide: 'sources',
+            nonSyntaxSide: 'targets',
+          },
+
+          secondary: {
+            mappedSide: 'targets',
+            nonMappedSide: 'sources',
+          },
+        }
+      );
+
+      console.log(result.children[0].children[0].children[0]);
+      // console.log(JSON.stringify(result, null, 2));
+      expect(
+        result.children[0].children[0].children[0].content.alignedWordIds
+      ).toEqual(['nvi_2']);
     });
   });
 });
--- src/features/treedown/syntaxMapper.ts
+++ src/features/treedown/syntaxMapper.ts
@@ -1,27 +1,54 @@
 import { SyntaxRoot, SyntaxNode, Alignment, Link } from 'structs';
 
+type AlignmentSide = 'sources' | 'targets';
+
+interface AlignmentPolarity {
+  primary: {
+    syntaxSide: AlignmentSide;
+    nonSyntaxSide: AlignmentSide;
+  };
+
+  secondary: {
+    mappedSide?: AlignmentSide;
+    nonMappedSide?: AlignmentSide;
+  };
+}
+
 const mapAlignedWords = (
   sourceId: string,
   alignment: Alignment,
-  secondaryAlignment: Alignment | null
+  secondaryAlignment: Alignment | null,
+  ap: AlignmentPolarity
 ): string[] => {
-  //console.log(alignment.links);
   const matchedLinks = alignment.links.filter((link: Link) => {
-    return link.targets.includes(sourceId);
+    return link[ap.primary.syntaxSide].includes(sourceId);
   });
 
   let secondaryMatchedLinks: Link[] = [];
 
-  if (secondaryAlignment) {
-    //console.log('matched links?', matchedLinks);
+  if (
+    secondaryAlignment &&
+    ap.secondary !== undefined &&
+    ap.secondary.mappedSide !== undefined &&
+    ap.secondary.nonMappedSide !== undefined
+  ) {
     secondaryMatchedLinks = matchedLinks
       .map((matchedLink: Link) => {
         const secondaryLink = secondaryAlignment.links.find(
           (secondaryLink: Link) => {
-            return secondaryLink.sources.find((secondaryLinkSource: string) => {
-              //console.log(matchedLink.sources, secondaryLinkSource);
-              return matchedLink.sources.includes(secondaryLinkSource);
-            });
+            if (
+              ap.secondary !== undefined &&
+              ap.secondary.mappedSide !== undefined &&
+              ap.secondary.nonMappedSide !== undefined
+            ) {
+              return secondaryLink[ap.secondary.mappedSide].find(
+                (secondaryLinkMappedId: string) => {
+                  return matchedLink[ap.primary.nonSyntaxSide].includes(
+                    secondaryLinkMappedId
+                  );
+                }
+              );
+            }
           }
         );
 
@@ -31,11 +58,10 @@ const mapAlignedWords = (
   }
 
   if (secondaryMatchedLinks.length > 0) {
-    console.log(secondaryMatchedLinks);
     return secondaryMatchedLinks.reduce((acc, curr) => {
       return acc.concat(curr.targets);
     }, [] as string[]);
-}
+  }
 
   if (matchedLinks.length > 0) {
     return matchedLinks.reduce((acc, curr) => {
@@ -48,19 +74,26 @@ const mapAlignedWords = (
 const _syntaxMapper = (
   syntaxNode: SyntaxNode,
   alignment: Alignment,
-  secondaryAlignment: Alignment | null
+  secondaryAlignment: Alignment | null,
+  alignmentPolarity: AlignmentPolarity
 ): any => {
   if (syntaxNode.content?.elementType === 'w' && syntaxNode.content.n) {
     syntaxNode.content.alignedWordIds = mapAlignedWords(
       syntaxNode.content.n,
       alignment,
-      secondaryAlignment
+      secondaryAlignment,
+      alignmentPolarity
     );
   }
 
   if (syntaxNode.children.length > 0) {
     return syntaxNode.children.map((childSyntaxNode: SyntaxNode) => {
-      return _syntaxMapper(childSyntaxNode, alignment, secondaryAlignment);
+      return _syntaxMapper(
+        childSyntaxNode,
+        alignment,
+        secondaryAlignment,
+        alignmentPolarity
+      );
     });
   }
   return syntaxNode;
@@ -69,45 +102,118 @@ const _syntaxMapper = (
 // Search for many:many links and refactor them.
 // That is, split them so that they are 1:1 or 1:many.
 // Group by array order.
-const refactorLinks = (links: Link[]) => {
-  return links
-    .map((link) => {
-      if (link.targets.length > 1) {
-        // MANY:1 || MANY
-        // Split links to 1:MANY
-        return link.targets.map((target: string, index: number) => {
+const refactorLinks = (
+  primaryLinks: Link[],
+  secondaryLinks: Link[] | undefined,
+  alignmentPolarity: AlignmentPolarity
+): [Link[], Link[] | null] => {
+  const refactoredPrimaryLinks = primaryLinks.map((link) => {
+    if (link[alignmentPolarity.primary.syntaxSide].length > 1) {
+      // MANY:1 || MANY
+      // Split links to 1:MANY
+      return link[alignmentPolarity.primary.syntaxSide].map(
+        (syntaxSideId: string, index: number) => {
           if (
-            link.targets.length === index + 1 &&
-            link.sources.length > link.targets.length
+            link[alignmentPolarity.primary.syntaxSide].length === index + 1 &&
+            link[alignmentPolarity.primary.nonSyntaxSide].length >
+              link[alignmentPolarity.primary.syntaxSide].length
           ) {
             // For the last target, grab all remaining sources.
             return {
-              sources: [...link.sources].splice(index),
-              targets: [target],
+              [alignmentPolarity.primary.syntaxSide]: [syntaxSideId],
+              [alignmentPolarity.primary.nonSyntaxSide]: [
+                ...link[alignmentPolarity.primary.nonSyntaxSide],
+              ].splice(index),
             };
           }
-          if (link.sources[index]) {
-            return { sources: [link.sources[index]], targets: [target] };
+          if (link[alignmentPolarity.primary.nonSyntaxSide][index]) {
+            return {
+              [alignmentPolarity.primary.nonSyntaxSide]: [
+                link[alignmentPolarity.primary.nonSyntaxSide][index],
+              ],
+              [alignmentPolarity.primary.syntaxSide]: [syntaxSideId],
+            };
           }
-        });
+        }
+      );
+    }
+    return link;
+  });
+
+  const refactoredSecondaryLinks = secondaryLinks?.map((link) => {
+    if (
+      alignmentPolarity.secondary !== undefined &&
+      alignmentPolarity.secondary.mappedSide !== undefined &&
+      alignmentPolarity.secondary.nonMappedSide !== undefined
+    ) {
+      const mappedSide = link[alignmentPolarity.secondary.mappedSide];
+      const nonMappedSide = link[alignmentPolarity.secondary.nonMappedSide];
+
+      if (mappedSide && nonMappedSide) {
+        if (mappedSide.length > 1) {
+          // MANY:1 || MANY
+          // Split links to 1:MANY
+          return mappedSide.map((mappedId: string, index: number) => {
+            if (
+              mappedSide.length === index + 1 &&
+              nonMappedSide.length > mappedSide.length
+            ) {
+              // For the last target, grab all remaining sources.
+              return {
+                [String(alignmentPolarity.secondary.nonMappedSide)]: [
+                  ...nonMappedSide.splice(index),
+                ],
+                [String(alignmentPolarity.secondary.mappedSide)]: [mappedId],
+              };
+            }
+            if (nonMappedSide[index]) {
+              return {
+                [String(alignmentPolarity.secondary.nonMappedSide)]: [
+                  nonMappedSide[index],
+                ],
+                [String(alignmentPolarity.secondary.mappedSide)]: [mappedId],
+              };
+            }
+          });
+        }
+        return link;
       }
-      return link;
-    })
-    .flat()
-    .filter((x): x is Link => Boolean(x));
+    }
+  });
+
+  const clean = (links: any[]): Link[] => {
+    return links.flat().filter((x): x is Link => Boolean(x) && x !== null);
+  };
+
+  return [
+    clean(refactoredPrimaryLinks),
+    secondaryLinks && refactoredSecondaryLinks
+      ? clean(refactoredSecondaryLinks)
+      : null,
+  ];
 };
 
 const syntaxMapper = (
   syntax: SyntaxRoot,
   alignment: Alignment,
-  secondaryAlignment: Alignment | null = null
+  secondaryAlignment: Alignment | null = null,
+  alignmentPolarity: AlignmentPolarity
 ) => {
-  const refactoredLinks = refactorLinks(alignment.links);
-  console.log('refactor', refactoredLinks);
+  const [refactoredPrimaryLinks, refactoredSecondaryLinks] = refactorLinks(
+    alignment.links,
+    secondaryAlignment?.links,
+    alignmentPolarity
+  );
+
+  const processedSecondaryAlignment = secondaryAlignment
+    ? { ...secondaryAlignment, links: refactoredSecondaryLinks ?? [] }
+    : null;
+
   _syntaxMapper(
     syntax,
-    { ...alignment, links: refactoredLinks },
-    secondaryAlignment
+    { ...alignment, links: refactoredPrimaryLinks },
+    processedSecondaryAlignment,
+    alignmentPolarity
   );
   return syntax;
 };
