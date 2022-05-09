aab0161f875fe3ca8517bda7f38aa5ea928a684e
Mike Brinker <themikejr@gmail.com>
2022-05-09 14:18:48 -0400

    Cleanup.

--- src/helpers/copySyntaxData.ts
+++ src/helpers/copySyntaxData.ts
@@ -1,24 +1,5 @@
-import { Corpus, SyntaxType } from 'structs';
+import { Corpus } from 'structs';
 
-//const copySyntaxData = (corpora: Corpus[]): Corpus[] => {
-//const corpusWithSyntax = corpora.find((corpus: Corpus): boolean => {
-//return Boolean(corpus.syntax);
-//});
-
-//return corpora.map((corpus: Corpus): Corpus => {
-//if (corpusWithSyntax && corpus.id !== corpusWithSyntax.id) {
-//const copiedSyntax = JSON.parse(JSON.stringify(corpusWithSyntax.syntax));
-
-//return {
-//...corpus,
-//syntax: copiedSyntax,
-//};
-//}
-//return corpus;
-//});
-//};
-//
-//
 const copySyntaxData = (corpora: Corpus[]) => {
   return corpora.map((corpus) => {
     if (corpus.syntax) {
