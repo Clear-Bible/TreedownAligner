653f425f1440a72a77ccc567371ab048d769c02a
Mike Brinker <themikejr@gmail.com>
2022-06-01 10:09:12 -0400

    Cleanup.

--- src/features/polyglot/index.tsx
+++ src/features/polyglot/index.tsx
@@ -1,20 +1,11 @@
-import React, { ReactElement } from 'react';
-import {
-  Card,
-  Container,
-  Stack,
-  Button,
-  ButtonGroup,
-  Typography,
-} from '@mui/material';
-import { Add, Remove } from '@mui/icons-material';
+import { ReactElement } from 'react';
+import { Card, Container, Stack, Typography } from '@mui/material';
 
 import { useAppSelector } from 'app/hooks';
 import useDebug from 'hooks/useDebug';
 import CorpusComponent from 'features/corpus';
-import { Corpus, CorpusViewport } from 'structs';
+import { CorpusViewport } from 'structs';
 
-// import cssVar from 'styles/cssVar';
 
 import './styles.css';
 
@@ -25,32 +16,6 @@ export const Polyglot = (): ReactElement => {
   const corpusViewports = useAppSelector((state) => {
     return state.app.corpusViewports;
   });
-
-  // const layoutRange = Array.from({ length: corpora.length }, (x, i) => i);
-
-  // const layout = layoutRange.map((key: number) => {
-  //   const width = 24 / corpora.length;
-  //   const x = width * key;
-  //
-  //   return {
-  //     i: `text_${key}`,
-  //     x,
-  //     y: 0,
-  //     w: width === 24 ? 8 : width,
-  //     h: 12,
-  //     minW: width,
-  //     maxW: width,
-  //     isResizable: false,
-  //   };
-  // });
-  //
-  // const theme = useAppSelector((state) => {
-  //   return state.app.theme;
-  // });
-  //
-  // if (corpusViewports.length === 0) {
-  //   return <Typography>To begin, add a corpus viewport.</Typography>;
-  // }
 
   return (
     <Stack
